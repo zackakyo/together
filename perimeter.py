d59082239b818880069b48962c4447d289121fb3
Bruno Defalquegit <defalquebruno@gmail.com>
2021-04-23 15:13:02 +0200

    ajouts des fonctions calcul de périmètre

---
+++ perimeter.py
@@ -0,0 +1,9 @@
+import math
+def RectanglePerimeter(_longueur, _largeur):
+	return (_longueur + _largeur) * 2
+def CirclePerimeter(_rayon):
+	return _rayon * 2 * math.pi
+def SquarePerimeter(_cote):
+	return _cote * 4
+def TrianglePerimeter(_cote1, _cote2, _cote3):
+	return _cote1 + _cote2 _cote3
