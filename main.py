8528e82a9387dfedc92e60f9dd20b4bc72c67881
TCHEUYASSI ISAAC <tcheuyassiisaac@yahoo.com>
2021-04-23 15:06:29 +0200

    Updates

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ print("area.py")
 print("=======")
 print("L'aire du carré de 4 cm de côté = " + str(area.SquareArea(4)))
 print("L'aire du rectangle de longueur 7 cm et 4 cm de largeur = " + str(area.RectangleArea(7,4)))
+
+print("L'aire du cercle ayant un rayon de 4 cm = " + str(area.CircleArea(4)))
+print("L'aire du triangle ayant une base de 4 cm et une hauteur de 7 cm = " + str(area.TriangleArea(4,7)))
+
+print("perimeter.py")
+print("============")
+print("Le périmètre du carré de 4 cm de côté = " + str(perimeter.SquarePerimeter(4)))
+print("L'aire du rectangle de longueur 7 cm et 4 cm de largeur = " + str(perimeter.RectanglePerimeter(7,4)))
+print("L'aire du cercle ayant un rayon de 4 cm = " + str(perimeter.CirclePerimeter(4)))
+print("L'aire du triangle ayant pour côtés 14, 7 et 9 cm  = " + str(perimeter.TrianglePerimeter(14,7,9)))
